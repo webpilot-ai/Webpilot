319cb861dfca8a6db477da544104d953a2b5d584
Noodles Zhuge <chegankang@gmail.com>
2023-04-27 23:58:27 -0400

    feat: add new feature to homepage

---
+++ src/options/AdvancedView.vue
@@ -0,0 +1,169 @@
+<template>
+  <div :class="advanced.main">
+    <div :class="[advanced.api, advanced.panel]">
+      <span :class="advanced.title"> API Settings </span>
+      <label for="provider">Active API Provider</label>
+      <select id="provider" name="provider">
+        <option selected value="open_ai_3.5">OpenAI gpt-3.5-terbo</option>
+        <option value="open_ai_4.0">OpenAI gpt-4.0-terbo</option>
+        <option value="baidu_wenxin">百度文心</option>
+      </select>
+
+      <label for="keys">Your API Key</label>
+      <input id="keys" name="keys" :placeholder="placeholder" type="text" />
+
+      <span :class="advanced.links"
+        >Visit: <a :href="links" target="_blank">{{ links }}</a></span
+      >
+      <div :class="advanced.host">
+        <div :class="advanced.selfHost">
+          <input id="self_host" name="self_host" type="checkbox" /><label for="self_host"
+            >Self Host</label
+          >
+        </div>
+        <div :class="advanced.more"><span :class="advanced.question_mark"></span> More help</div>
+      </div>
+
+      <div>
+        <button :class="advanced.saveButton" @click="save()">SAVE CHANGES</button>
+      </div>
+    </div>
+
+    <div :class="[advanced.extension, advanced.panel]">
+      <span :class="advanced.title"> Extension Settings </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const placeholder = ref('Enter your API Key from OpenAI')
+const links = ref('https://platform.openai.com/account/api-keys')
+
+const save = () => {
+  console.log(' form saved !')
+}
+</script>
+
+<style module="advanced" lang="scss">
+.openAiLogo {
+  width: 22px;
+  height: 22px;
+  background-image: url('./images/openAiLogo.png');
+}
+
+.main {
+  margin-top: 20px;
+  padding: 24px 16px;
+  font-family: 'PingFang SC', Helvetica, Arial, sans-serif;
+}
+
+.panel {
+  display: flex;
+  flex-direction: column;
+  padding: 24px 16px;
+  background-color: white;
+  border-radius: 10px;
+
+  &:last-child {
+    margin-top: 27px;
+  }
+
+  .title {
+    color: #4f5aff;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 34px;
+  }
+
+  label {
+    margin-top: 24px;
+    color: #292929;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 25px;
+  }
+
+  select,
+  input {
+    width: 360px;
+    height: 36px;
+    margin-top: 8px;
+    padding-left: 8px;
+    font-size: 14px;
+    line-height: 20px;
+    border: 1px solid #dcdee1;
+    border-radius: 5px;
+  }
+
+  option {
+    font-size: 14px;
+    line-height: 20px;
+  }
+
+  .links {
+    margin-top: 8px;
+    font-size: 14px;
+    line-height: 20px;
+  }
+}
+
+.host {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 360px;
+  margin-top: 8px;
+}
+
+.selfHost {
+  display: flex;
+  flex-direction: row;
+
+  input {
+    width: 16px;
+    height: 16px;
+    margin-top: 0;
+  }
+
+  label {
+    margin-top: 0;
+    font-size: 14px;
+    line-height: 20px;
+  }
+}
+
+.more {
+  /* identical to box height */
+  color: #585b58;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: right;
+  text-decoration-line: underline;
+
+  .question_mark {
+    background-color: #4f5aff;
+    background-image: url('./images/question_mark.svg');
+    background-size: cover;
+  }
+}
+
+.saveButton {
+  margin-top: 100px;
+  padding: 8px 16px;
+  color: white;
+  font-weight: 500;
+  font-size: 14px;
+  font-family: 'PingFang SC', Helvetica, Arial, sans-serif;
+  line-height: 20px;
+  background-color: #929497;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.extension {
+  color: yellow;
+}
+</style>
--- src/options/index.vue
+++ src/options/index.vue
@@ -1,13 +1,154 @@
 <template>
-  <div :class="$style.container">Options</div>
+  <div :class="index.container">
+    <div :class="index.main">
+      <!-- header -->
+      <div :class="index.header">
+        <div :class="index.logo">
+          <img alt=" logog" src="./images/Text+Logo.svg" />
+          <span :class="index.slogan">Opensource_ai_assistant_on_all_websites</span>
+        </div>
+        <ul :class="index.tabs">
+          <li
+            :class="[index.tab, active == 'advanced' ? index.active : '']"
+            @click="active = 'advanced'"
+          >
+            Advanced
+          </li>
+          <li :class="[index.tab, active == 'about' ? index.active : '']" @click="active = 'about'">
+            About
+          </li>
+        </ul>
+      </div>
+
+      <!-- body -->
+      <div :class="index.body">
+        <AdvancedView v-show="active == 'advanced'" />
+
+        <AboutView v-show="active == 'about'" />
+      </div>
+
+      <!-- footer -->
+      <div :class="index.footer">
+        <span>Webpilot is open source</span>
+        <a href="https://github.com/Fluentify-IO/Fluentify" rel="noreferrer" target="_blank">
+          Star on Github
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import '@assets/styles/reset.scss'
+import {ref} from 'vue'
+
+import AdvancedView from './AdvancedView.vue'
+import AboutView from './AboutView.vue'
+
+const active = ref('advanced')
 </script>
 
-<style lang="scss" module>
+<style module="index" lang="scss">
 .container {
-  color: #f00;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(150.76deg, #efdaff 12.93%, #b28aff 64.87%, #6f63ff 108.73%);
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: calc(100vw - 18px);
+  max-width: 1438px;
+  height: calc(100vh - 18px);
+  padding: 24px 16px 16px;
+  overflow-y: scroll;
+  background: rgb(255 255 255 / 60%);
+  border: 1px solid #fff;
+  border-radius: 20px 20px 0 0;
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  .body {
+    justify-content: flex-start;
+    min-height: calc(100vh - 50px);
+  }
+}
+
+.logo {
+  display: flex;
+  flex-direction: row;
+
+  img {
+    margin-right: 24px;
+  }
+
+  .slogan {
+    display: flex;
+    align-items: center;
+    color: #777;
+    font-weight: 400;
+    font-size: 18px;
+    font-style: normal;
+    line-height: 25px;
+  }
+}
+
+@media (width <= 820px) {
+  .slogan {
+    display: none;
+  }
+}
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  line-height: 25px;
+
+  span {
+    margin-top: 32px;
+    color: #929497;
+  }
+
+  a {
+    color: #4f5aff;
+    text-decoration: none;
+  }
+}
+
+.tabs {
+  display: flex;
+  flex-direction: row;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+  list-style: none;
+  padding-inline-start: 24px;
+}
+
+li + li {
+  margin-left: 48px;
+}
+
+.tab {
+  border-bottom: 4px solid rgb(255 255 255 / 0%);
+  cursor: pointer;
+}
+
+.active {
+  border-bottom: 4px solid #4f5aff;
 }
 </style>
